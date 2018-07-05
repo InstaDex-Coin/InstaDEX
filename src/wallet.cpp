16c3f538bdf81f5273999d6e5561173725608a48
InstaDex-Coin <40644761+InstaDex-Coin@users.noreply.github.com>
2018-07-05 17:38:46 +0100

    Re - Allow "address" to be shown in gettransaction RPC command

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1024,11 +1024,11 @@ void CWalletTx::GetAmounts(list<COutputEntry>& listReceived,
 
         // In either case, we need to get the destination address
         CTxDestination address;
-        //if (!ExtractDestination(txout.scriptPubKey, address)) {
-            //LogPrintf("CWalletTx::GetAmounts: Unknown transaction type found, txid %s\n",
-                //this->GetHash().ToString());
-            //address = CNoDestination();
-        //}
+        if (!ExtractDestination(txout.scriptPubKey, address)) {
+            LogPrintf("CWalletTx::GetAmounts: Unknown transaction type found, txid %s\n",
+                this->GetHash().ToString());
+            address = CNoDestination();
+        }
 
         COutputEntry output = {address, txout.nValue, (int)i};
 
