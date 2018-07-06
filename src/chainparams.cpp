c50a8d57d2fb0b95871470a96b06a64a4dbafb6f
InstaDex-Coin <40644761+InstaDex-Coin@users.noreply.github.com>
2018-07-06 15:52:40 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,7 +162,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        fRequireRPCPassword = false;
+        fRequireRPCPassword = true;
         fMiningRequiresPeers = false;
         fAllowMinDifficultyBlocks = false;
         fDefaultConsistencyChecks = false;
